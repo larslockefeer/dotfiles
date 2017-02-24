25f9ff24a98c5bee9e2df5faebf0ff77eee55b8a
Lars Lockefeer <lars.lockefeer@teampicnic.com>
2017-02-24 11:06:28 +0100

    Added zsh config

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles          # dotfiles directory
 olddir=~/dotfiles_old   # old dotfiles backup directory
-files="vimrc ctags"  	# list of files/folders to symlink in homedir
+files="zshrc vimrc ctags"  	# list of files/folders to symlink in homedir
 
 ##########
 
