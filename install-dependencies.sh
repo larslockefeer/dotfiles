dcfa6993ae02abf52a4cd96b1798f90dd5fcd75f
Lars Lockefeer <lars.lockefeer@gmail.com>
2019-04-10 10:56:37 +0200

    More fixes

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -17,6 +17,17 @@ which brew >/dev/null 2>&1 || (
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 )
 
+# GNUPG & GNUPG 2
+which gnupg >/dev/null 2>&1 || (
+  echo "Installing gnupg";
+  brew install gnupg
+)
+which gnupg2 >/dev/null 2>&1 || (
+  echo "Installing gnupg 2";
+  brew install gnupg2
+)
+
+# The Fuck
 which thefuck >/dev/null 2>&1 || (
   echo "Installing thefuck";
   brew install thefuck
@@ -69,7 +80,7 @@ which sdkmanager >/dev/null 2>&1 || (
 which rvm >/dev/null 2>&1 || (
   echo "Installing rvm";
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB;
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0 --with-openssl-dir=$HOME/.rvm/usr;
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0;
 )
 
 # Bundler
